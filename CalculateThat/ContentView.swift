0f4ea15b5ef16112458feb67ed8b1498e79520b5
Chris Walter <walt50@gmail.com>
2019-12-17 21:52:12 -0500

    Initial Commit

---
+++ CalculateThat/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CalculateThat
+//
+//  Created by Christopher Walter on 12/17/19.
+//  Copyright © 2019 Christopher Walter. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
