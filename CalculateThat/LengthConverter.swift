49082c77a09b7fd73f6016a560d74f77a97031b1
Chris Walter <walt50@gmail.com>
2020-04-12 21:39:02 -0400

    Added TabBar with WeSplit (Tip Calc), and new length converter.

---
+++ CalculateThat/LengthConverter.swift
@@ -0,0 +1,21 @@
+//
+//  LengthConverter.swift
+//  CalculateThat
+//
+//  Created by Christopher Walter on 4/12/20.
+//  Copyright © 2020 Christopher Walter. All rights reserved.
+//
+
+import SwiftUI
+
+struct LengthConverter: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LengthConverter_Previews: PreviewProvider {
+    static var previews: some View {
+        LengthConverter()
+    }
+}
---
+++ CalculateThat/TipCalcView.swift
@@ -0,0 +1,81 @@
+//
+//  TipCalcView.swift
+//  CalculateThat
+//
+//  Created by Christopher Walter on 4/12/20.
+//  Copyright © 2020 Christopher Walter. All rights reserved.
+//
+
+import SwiftUI
+
+struct TipCalcView: View {
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 2
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2) // add 2 because range is from 2 - 100, but indexes are 0 - 98
+        let tipSelection = Double(tipPercentages[tipPercentage])
+        let orderAmount = Double(checkAmount) ?? 0.0
+        let tipValue = orderAmount / 100 * tipSelection
+        let grandTotal = orderAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+
+        return amountPerPerson
+    }
+    
+    var checkTotal: Double {
+        let orderAmount = Double(checkAmount) ?? 0
+        let tipAmount = Double(tipPercentages[tipPercentage]) / 100 * orderAmount
+        let grandTotal = orderAmount + tipAmount
+        return grandTotal
+    }
+
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", text: $checkAmount)
+                        .keyboardType(.decimalPad)
+                    Picker("Number of people", selection: $numberOfPeople) {
+                            ForEach(2 ..< 100) {
+                                Text("\($0) people")
+                        }
+                    }
+                }
+                Section(header: Text("Check Total")
+                    .bold()
+                ) {
+                    Text("$\(checkTotal, specifier: "%.2f")")
+                }
+                Section(header: Text("Amount per Person")
+                    .bold()
+                ) {
+                    Text("$\(totalPerPerson, specifier: "%.2f")")
+                }
+                Section {
+                    Text("How much tip do you want to leave?")
+                    Picker("Top Percentage", selection: $tipPercentage) {
+                        ForEach(0..<self.tipPercentages.count)
+                        {
+                            Text("\(self.tipPercentages[$0])%")
+                        }
+                    }
+                .pickerStyle(SegmentedPickerStyle())
+                }
+            }
+        .navigationBarTitle("WeSplit")
+        }
+        
+    }
+    
+  
+}
+
+struct TipCalcView_Previews: PreviewProvider {
+    static var previews: some View {
+        TipCalcView()
+    }
+}
+
